34df24dea1b01352e1f0fde11b4e631dd40596ff
vladtermene <vlad@termene.ro>
2022-07-21 08:38:48 +0300

    Synch with personal machine

--- start.sh
+++ start.sh
@@ -1,2 +1,2 @@
-uvicorn inference:app --host 127.0.0.1 --port 8001 #--reload
-# uvicorn inference:app --host 0.0.0.0 --port 8001 #--reload
+# uvicorn inference:app --host 127.0.0.1 --port 8001 #--reload
+uvicorn inference:app --host 0.0.0.0 --port 8001 #--reload
